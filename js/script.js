56cf035f911689ac03406daecb7ea36ec1d528be
DarioDC95 <dario_95@live.com>
2022-11-24 16:36:37 +0100

    Beginning Exercise

--- js/script.js
+++ js/script.js
@@ -1,3 +1,13 @@
+// GENERARE UNA PASSWORD CASUALE
+
 // Chiedo il nome
 let ask_name = prompt("Inserisci il tuo nome");
-console.log(ask_name)
+console.log(prompt("Inserisci il tuo nome"));
+// Chiedo il cognome
+let ask_surname = prompt("Inserisci il tuo cognome");
+console.log(ask_surname);
+// Chiedo il colore preferito
+let ask_favourite_color = prompt("Inserisci il tuo colore preferito");
+console.log(ask_favourite_color);
+// Collido i valori delle variabili precedenti
+let
