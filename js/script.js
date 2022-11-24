bd9f57748ccf22261584d56e62e2ee1866eb80a5
DarioDC95 <dario_95@live.com>
2022-11-24 16:50:17 +0100

    Exercise Done

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@
 
 // Chiedo il nome
 let ask_name = prompt("Inserisci il tuo nome");
-console.log(prompt("Inserisci il tuo nome"));
+console.log(ask_name);
 // Chiedo il cognome
 let ask_surname = prompt("Inserisci il tuo cognome");
 console.log(ask_surname);
@@ -10,4 +10,8 @@ console.log(ask_surname);
 let ask_favourite_color = prompt("Inserisci il tuo colore preferito");
 console.log(ask_favourite_color);
 // Collido i valori delle variabili precedenti
-let
+let password_result = ask_name + ask_surname + ask_favourite_color + "21"
+console.log(password_result)
+// Inserisco il risultato nell'HTML
+document.getElementById("password").innerHTML = document.getElementById("password").innerHTML + " " + password_result
+console.log(document.getElementById("password").innerHTML)
