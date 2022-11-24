fa6eb263ef4340b5b2e11b42d158c1c6f13700e9
DarioDC95 <dario_95@live.com>
2022-11-24 17:48:50 +0100

    Added the replace for the eventual space in the surname

--- js/script.js
+++ js/script.js
@@ -6,12 +6,14 @@ console.log(ask_name);
 // Chiedo il cognome
 let ask_surname = prompt("Inserisci il tuo cognome");
 console.log(ask_surname);
+let nospace_surname = ask_surname.replace(" ", "_");
+console.log(nospace_surname);
 // Chiedo il colore preferito
 let ask_favourite_color = prompt("Inserisci il tuo colore preferito");
 console.log(ask_favourite_color);
 // Collido i valori delle variabili precedenti
-let password_result = ask_name + ask_surname + ask_favourite_color + "21"
-console.log(password_result)
+let password_result = ask_name + nospace_surname + ask_favourite_color + "21";
+console.log(password_result);
 // Inserisco il risultato nell'HTML
 document.getElementById("password").innerHTML = document.getElementById("password").innerHTML + " " + password_result
-console.log(document.getElementById("password").innerHTML)
+console.log(document.getElementById("password").innerHTML);
